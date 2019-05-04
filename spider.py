5ca654a0019801336a1262a6da55dba23fdd4f3f
apple <491565693@qq.com>
2019-05-04 15:21:38 +0800

    修改工作路径问题

--- spider.py
+++ spider.py
@@ -1,20 +1,24 @@
 from utils.common import *
 from bs4 import BeautifulSoup
 import utils.database
+import urllib3
+import os
 
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+os.chdir(os.path.dirname(__file__))
 db = utils.database.DBTool('./fofa.db')
 url = "https://fofa.so/about_client"
 
 html = http_request_get(url)
-bs = BeautifulSoup(html.text,'lxml')
+bs = BeautifulSoup(html.text, 'lxml')
 poc = bs.select('.poc_row li')
 ob = []
 res = []
 T = db.executeQuery('select * from data order by ID desc')
 index = T.fetchall()[0][1]
 
-for i,j in enumerate(poc):
-    if i%4 == 0 and i != 0:
+for i, j in enumerate(poc):
+    if i % 4 == 0 and i != 0:
         ob.append(tuple(res))
         res = []
     if j.a:
@@ -28,3 +32,6 @@ for i,j in enumerate(poc):
 sql = 'insert into data (POC_NAME, POC_DETAIL, HARM, RULE) values (?,?,?,?)'
 T = db.executeUpdate(sql, ob)
 db.close()
+
+if __name__ == '__main__':
+    os.chdir(os.path.dirname(__file__))
